8b3876d4d92f03d0d84c9e15b889bcf47965d895
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-03-22 21:50:36 -0300

    feat: create tables

--- apps/management_employee/models.py
+++ apps/management_employee/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.utils import timezone
+from datetime import date
 
-# Create your models here.
+
+class Emp(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    recovery_answer = models.CharField(max_length=20)
+    recovery_email = models.CharField(max_length=50)
+    created_on = models.DateTimeField(default=timezone.now)
+    last_updated_on = models.DateTimeField(blank=True, null=True)
+    deleted_on = models.DateTimeField(blank=True, null=True)
+    first_name = models.CharField(max_length=40)
+    middle_name = models.CharField(max_length=40, blank=True, default='')
+    last_name = models.CharField(max_length=40)
+    phone_number = models.IntegerField(blank=True, null=True)
+    created_on = models.DateTimeField(default=timezone.now)
+    deleted_on = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Employee(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    recovery_answer = models.CharField(max_length=20)
+    recovery_email = models.CharField(max_length=20)
+    created_on = models.DateTimeField(default=timezone.now)
+    last_updated_on = models.DateTimeField(blank=True, null=True)
+    deleted_on = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return "ID: %d Username: %s Password: %s" % (self.id, self.username, self.password)
+
+
+class EmployeeInfo(models.Model):
+    employee = models.ForeignKey('Employee', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=40)
+    middle_name = models.CharField(max_length=40, blank=True, default='')
+    last_name = models.CharField(max_length=40)
+    department = models.CharField(max_length=50, default='')
+    phone_number = models.IntegerField(blank=True, null=True)
+    created_on = models.DateTimeField(default=timezone.now)
+    deleted_on = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return "ID: %d First Name: %s Middle Name: %s Last Name: %s Phone Number: %s" % (self.id, self.first_name, self.middle_name, self.last_name, self.phone_number)
+
+
+class Status(models.Model):
+    types = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "%d %s" % (self.id, self.types)
+
+
+class Work(models.Model):
+    employee = models.ForeignKey('Employee', on_delete=models.CASCADE)
+    work_status = models.CharField(max_length=30)
+    notes = models.CharField(max_length=100, default='', blank=True)
+    time = models.TimeField(default=timezone.localtime)
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return "ID: %d Work Status: %s Notes: %s Time: %s Date: %s" % (
+        self.id, self.work_status, self.notes, self.time, self.date)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return 'username: {}, password: {}'.format(self.username, self.password)
